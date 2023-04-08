8b3eb9edf64422718648460471f682c688b4dcca
agahbabak <agahbabak@gmail.com>
2023-04-08 10:40:38 +0330

    remove postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
@@ -12,22 +11,9 @@ import { VariationsModule } from './variations/variations.module';
 import { UnitsModule } from './units/units.module';
 import { AuthModule } from './auth/auth.module';
 
-const defaultOptions: TypeOrmModuleOptions = {
-  type: 'postgres',
-  port: 5432,
-  username: 'root',
-  password: 'secret',
-  database: 'store',
-  entities: [],
-  synchronize: true,
-};
-
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/store'),
-    TypeOrmModule.forRoot({
-      ...defaultOptions,
-    }),
     UsersModule,
     ProductsModule,
     StatesModule,
